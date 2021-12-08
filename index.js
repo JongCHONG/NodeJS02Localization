5c1b78ecbd4111179f1869acbb89a3ca5232bb46
JongCHONG <yonhoa27@hotmail.fr>
2021-12-08 10:52:26 +0100

    localization

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 
 app.get("/:lang?", (req, res) => {
-  const { lang } = req.params
+  const { lang } = req.params //pour recupérer :lang
+  //req.query pour récupérer ?username&password
 
   res.render('home', {
     translation,
-    lang
+    isFr: lang === "fr",
+    isEn: lang === "en",
+    isEs: lang === "es"
   })
 })
 
